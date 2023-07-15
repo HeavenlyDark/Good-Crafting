e71ea7e72e0200086ff73feda737c7cc1b866730
HeavenlyDark <106950115+HeavenlyDark@users.noreply.github.com>
2023-07-15 16:19:21 -0500

    Update portales.js

--- static/js/portales.js
+++ static/js/portales.js
@@ -12,30 +12,44 @@ var grupo2CasilleroTexto = document.getElementById("grupo2CasilleroTexto");
 
 // Función para actualizar el Grupo 1
 function actualizarGrupo1() {
-  var valor1 = grupo1Casillero1.value;
-  var valor2 = grupo1Casillero2.value;
-  var valor3 = grupo1Casillero3.value;
+  var valores = grupo1Casillero1.value.split(" ");
+  if (valores.length === 3) {
+    grupo1Casillero1.value = valores[0];
+    grupo1Casillero2.value = valores[1];
+    grupo1Casillero3.value = valores[2];
+  }
+
+  var valor1 = parseFloat(grupo1Casillero1.value) || 0;
+  var valor2 = parseFloat(grupo1Casillero2.value) || 0;
+  var valor3 = parseFloat(grupo1Casillero3.value) || 0;
 
   grupo1CasilleroTexto.textContent = valor1 + " " + valor2 + " " + valor3;
 
   // Actualizar los valores del Grupo 2
   grupo2Casillero1.value = valor1 / 8;
-  grupo2Casillero2.value = valor2/1;
+  grupo2Casillero2.value = valor2;
   grupo2Casillero3.value = valor3 / 8;
   actualizarGrupo2();
 }
 
 // Función para actualizar el Grupo 2
 function actualizarGrupo2() {
-  var valor1 = grupo2Casillero1.value;
-  var valor2 = grupo2Casillero2.value;
-  var valor3 = grupo2Casillero3.value;
+  var valores = grupo2Casillero1.value.split(" ");
+  if (valores.length === 3) {
+    grupo2Casillero1.value = valores[0];
+    grupo2Casillero2.value = valores[1];
+    grupo2Casillero3.value = valores[2];
+  }
+
+  var valor1 = parseFloat(grupo2Casillero1.value) || 0;
+  var valor2 = parseFloat(grupo2Casillero2.value) || 0;
+  var valor3 = parseFloat(grupo2Casillero3.value) || 0;
 
   grupo2CasilleroTexto.textContent = valor1 + " " + valor2 + " " + valor3;
 
   // Actualizar los valores del Grupo 1
   grupo1Casillero1.value = valor1 * 8;
-  grupo1Casillero2.value = valor2*1;
+  grupo1Casillero2.value = valor2;
   grupo1Casillero3.value = valor3 * 8;
   actualizarGrupo1();
 }
@@ -50,3 +64,41 @@ function copiarContenido(elementId) {
   document.execCommand("copy");
   window.getSelection().removeAllRanges();
 }
+
+// Evento para detectar pegado de texto en los casilleros del Grupo 1
+grupo1Casillero1.addEventListener("input", function(event) {
+  var valores = grupo1Casillero1.value.split(" ");
+  if (valores.length === 3) {
+    grupo1Casillero1.value = valores[0];
+    grupo1Casillero2.value = valores[1];
+    grupo1Casillero3.value = valores[2];
+    actualizarGrupo1();
+  }
+});
+
+grupo1Casillero2.addEventListener("input", function(event) {
+  actualizarGrupo1();
+});
+
+grupo1Casillero3.addEventListener("input", function(event) {
+  actualizarGrupo1();
+});
+
+// Evento para detectar pegado de texto en los casilleros del Grupo 2
+grupo2Casillero1.addEventListener("input", function(event) {
+  var valores = grupo2Casillero1.value.split(" ");
+  if (valores.length === 3) {
+    grupo2Casillero1.value = valores[0];
+    grupo2Casillero2.value = valores[1];
+    grupo2Casillero3.value = valores[2];
+    actualizarGrupo2();
+  }
+});
+
+grupo2Casillero2.addEventListener("input", function(event) {
+  actualizarGrupo2();
+});
+
+grupo2Casillero3.addEventListener("input", function(event) {
+  actualizarGrupo2();
+});
